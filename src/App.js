bc1d7a7d14cb94f4e14346df5915f8fefe7750cd
JulianBarbossa9 <julianbarbossa@hotmail.com>
2021-11-27 12:37:53 -0500

    first commit breaking bad

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 2rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background:-webkit-linear-gradient(top left, #007d35 0%, #007d35 20%, #0f574e 100%);
+  background-size:200px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  padding: 1rem 3rem;
+  margin: 3rem 0;
+  font-size: 2rem;
+  border: 2px solid black;
+
+`;
 
 function App() {
+  
+  const consultarAPI = () => {
+
+    const resultado = fetch('https://breakingbadquotes.xyz'); // (fetch) toma la url a la que vamos hacer la consulta, este métod sirve para la petición a servidor 
+    const frase = resultado.then(respuesta => {
+      return respuesta.json();
+    }) ;
+
+    console.log(frase); 
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Contenedor>
+      <Button
+        // onClick={consultarApi}
+        onClick = {() => consultarAPI()}
+      >
+        Get Phrase
+      </Button>
+   </Contenedor>
   );
 }
 
